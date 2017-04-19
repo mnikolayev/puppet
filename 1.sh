237faec002527621fc32267eacc3866abe4c35f7
Mike <doubledeckwhooper@gmail.com>
2017-04-19 13:34:42 +0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,5 +2,4 @@
 yum install -y epel-release
 yum install -y puppet
 service puppet start
-puppet apply -e 'include final' --modulepath=/vagrant
-              
+puppet apply -e 'include final' --modulepath=/vagrant --debug
